f2fc18e9f779218a5abed5f55edbf83469a69d58
Naveed <naveed235812@gmail.com>
2024-05-26 22:34:49 +0200

    code clean up

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as chalk from "chalk";
-import { Main } from "@naveed235812/hello-world-windows/lib/Main";
+import { CreateMsgString } from "@naveed235812/hello-world-windows/lib/Main";
 
-export function AdvanceVariantPrint(subStr: string) {
-  console.log("Display Msg: " + subStr);
-  Main("Hello-World Component");
+export function AdvanceVariantPrint(appMsgString: string): void {
+  const domainComponentMsg = CreateMsgString();
+  console.log(appMsgString + " --> " + domainComponentMsg);
 }
